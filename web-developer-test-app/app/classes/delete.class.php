af0217f9970d448535b51a34c36bd40a1923fde1
S-Arturs <88853777+S-Arturs@users.noreply.github.com>
2021-09-08 12:58:42 +0300

    Update delete.class.php

--- web-developer-test-app/app/classes/delete.class.php
+++ web-developer-test-app/app/classes/delete.class.php
@@ -2,7 +2,7 @@
 //delete button functionality
 include '../autoloader.inc.php';
 $_POST["session"];
-$deletedEmail = new emailcontroller();
+$deletedEmail = new EmailController();
 $deletedEmail->removeUsers($_POST["session"]);
 header("Location: ../../public/emails-table.php");
 exit;
